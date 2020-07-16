e8acadc9365de7d2b6121b4b70b31930d2c1d076
glolorun <63762681+glolorun@users.noreply.github.com>
2020-07-16 13:55:00 -0400

    Login up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :categories
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
   resources :users, only: [:create, :show, :index] do
     resources :posts
   end
