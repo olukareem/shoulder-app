a5290cefa841cb898e07355aff32ad8c42402f9e
glolorun <63762681+glolorun@users.noreply.github.com>
2020-07-21 17:18:23 -0400

    edit broken, kind of panicking

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :posts, dependent: :destroy
     has_secure_password
     validates :password, length: { minimum: 6 }, on: :create
-    validates :username, presence: true, uniqueness: true
+    validates :username, format: { with: /\A[a-zA-Z0-9]+\Z/ }, presence: true, uniqueness: true
     validates :email, presence: false, uniqueness: true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
--- client/src/React_pages/Categories.jsx
+++ client/src/React_pages/Categories.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { getOneCategory, addPost, getPosts } from "../services/apihelper";
-import { Link, } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 import Moment from "react-moment";
 
 export default class Categories extends Component {
@@ -15,29 +14,25 @@ export default class Categories extends Component {
     });
     const posts = await getPosts();
     this.setState({
-      posts
+      posts,
     });
-    
-      
   };
 
   render() {
-      const date = new Date();
-      console.log(this.state.posts)
-      console.log()
+    const date = new Date();
 
-      
-   const createdBy =                             this.state.posts &&
-    this.state.posts.map((post) => (
-        <div><p style={{fontSize:"0.633vw", color: "#747474",}}>
-         <strong>   Posted by{" "}</strong>
-            <Link to={`/profile/${post.user_id}`}>
-                {post.user.username}
-            </Link>
-        </p></div>
-    ))
+    const createdBy =
+      this.state.posts &&
+      this.state.posts.map((post) => (
+        <div>
+          <p style={{ fontSize: "0.633vw", color: "#747474" }}>
+            <strong> Posted by </strong>
+            <Link to={`/profile/${post.user_id}`}>{post.user.username}</Link>
+          </p>
+        </div>
+      ));
     return (
-      <div class="py-2 max-w-full" style={{margin: "0 19.667vw"}}>
+      <div class="py-2 max-w-full" style={{ margin: "0 19.667vw" }}>
         {this.state.category && (
           <div>
             <h1
@@ -56,7 +51,7 @@ export default class Categories extends Component {
               Category: {this.state.category.name}
             </h1>
             {this.state.category.posts.map((post) => (
-              <div style={{ padding: "1.3vw 2.667vw",  }}>
+              <div style={{ padding: "1.3vw 2.667vw" }}>
                 <ul
                   style={{
                     backgroundColor: "#F8F8F8",
@@ -65,39 +60,60 @@ export default class Categories extends Component {
                     flexDirection: "row",
                     flexWrap: "nowrap",
                     flexFlow: "row wrap",
-                            textAlign: "left",
-                            alignItems: "center",
-                    justifyContent:"center",
+                    textAlign: "left",
+                    alignItems: "center",
+                    justifyContent: "center",
                     // width: "44.4vw",
                     height: "20.067vw",
                     borderRadius: "0.8vw",
                     boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.48)",
-                            paddingTop: "1.067vw",
+                    paddingTop: "1.067vw",
 
-                            paddingLeft: "2.067vw",
-                            paddingRight: "2.067vw",
-                            overflow: "scroll",
-                    
+                    paddingLeft: "2.067vw",
+                    paddingRight: "2.067vw",
+                    overflow: "scroll",
                   }}
                 >
-                  <li style={{ width: "100%",}}>
-                    <p style={{fontWeight:"bold", color:"#000", fontSize:"1.4vw", padding: "0.267vw 0 0.757vw", lineHeight: "1.2vw"}}>{post.title}</p>
-                    <hr style={{border: "1px solid #DDDDDD"}}></hr>
+                  <li style={{ width: "100%" }}>
+                    <p
+                      style={{
+                        fontWeight: "bold",
+                        color: "#000",
+                        fontSize: "1.4vw",
+                        padding: "0.267vw 0 0.757vw",
+                        lineHeight: "1.2vw",
+                      }}
+                    >
+                      {post.title}
+                    </p>
+                    <hr style={{ border: "1px solid #DDDDDD" }}></hr>
                   </li>
-                  <li style={{padding: "0.767vw 0", width: "100%" }}>
-                    <p style={{color: "#747474", fontWeight: "350", fontSize:"0.833vw"}}><strong>Description: </strong>{post.description}</p>
-                        </li>
-                        <hr style={{border: "1px solid #DDDDDD", width: "100%"}}></hr>
-                        <li style={{padding: "0.767vw 0", width: "100%" }}>
-                    <p class="font-light" style={{fontSize:"0.733vw"}}>{post.body}</p>
-                        </li>
-                        <hr style={{border: "1px solid #DDDDDD", width: "100%"}}></hr>
+                  <li style={{ padding: "0.767vw 0", width: "100%" }}>
+                    <p
+                      style={{
+                        color: "#747474",
+                        fontWeight: "350",
+                        fontSize: "0.833vw",
+                      }}
+                    >
+                      <strong>Description: </strong>
+                      {post.description}
+                    </p>
+                  </li>
+                  <hr
+                    style={{ border: "1px solid #DDDDDD", width: "100%" }}
+                  ></hr>
+                  <li style={{ padding: "0.767vw 0", width: "100%" }}>
+                    <p class="font-light" style={{ fontSize: "0.733vw" }}>
+                      {post.body}
+                    </p>
+                  </li>
+                  <hr
+                    style={{ border: "1px solid #DDDDDD", width: "100%" }}
+                  ></hr>
 
-                        <li style={{padding: "0.767vw 0", width: "100%" }}>
-
-
-
-                            {/* {this.state.posts &&
+                  <li style={{ padding: "0.767vw 0", width: "100%" }}>
+                    {/* {this.state.posts &&
                                 this.state.posts.map((post) => (
                                     <p>
                                         Posted by{" "}
@@ -106,28 +122,26 @@ export default class Categories extends Component {
                                         </Link>
                                     </p>
                                 ))} */}
-{createdBy}
+                    {createdBy}
 
-                            
-
-
-
-                    <p style={{fontSize:"0.633vw", color: "#747474",}}><strong>
-                      Created:&nbsp;</strong>
+                    <p style={{ fontSize: "0.633vw", color: "#747474" }}>
+                      <strong>Created:&nbsp;</strong>
                       <Moment format="MMM D YYYY" withTitle>
                         {post.created_at}
                       </Moment>
-                      , <strong>last updated:</strong> <Moment fromNow ago>
+                      , <strong>last updated:</strong>{" "}
+                      <Moment fromNow ago>
                         {post.updated_at}
                       </Moment>
                       &nbsp;ago
                     </p>
                   </li>
-                  <hr style={{border: "1px solid #DDDDDD", width: "100%"}}></hr>
+                  <hr
+                    style={{ border: "1px solid #DDDDDD", width: "100%" }}
+                  ></hr>
 
-                  <li style={{padding: "0.767vw 0", width: "100%" }}>
-                    <p>
-                    </p>
+                  <li style={{ padding: "0.767vw 0", width: "100%" }}>
+                    <p></p>
                   </li>
                 </ul>
               </div>
