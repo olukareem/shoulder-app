1f552570977189582f1074921bd11ef8667aef7c
glolorun <63762681+glolorun@users.noreply.github.com>
2020-07-21 08:11:09 -0400

    Category posts page looking clean. Todo: Complete EDIT Action, Create Other Users Component, Styling/Media Queries, Clean Up Code, Style Header, Update Readme, Fix -posted by- in category and profile sections

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link, Route, Switch, withRouter } from "react-router-dom";
 import axios from "axios";
+import $ from 'jquery';
+
 import {
   loginUser,
   verifyUser,
@@ -68,7 +70,7 @@ class App extends Component {
     this.setState((prevState) => ({
       posts: [...prevState.posts, newPost],
     }));
-      window.location.reload(false)
+    window.location.reload(false);
   };
 
   loginSubmit = async (userData) => {
@@ -93,37 +95,55 @@ class App extends Component {
     this.props.history.push(`/profile/${currentUser.id}`);
   };
 
-    render() {
-//         var obj = {};
-//         const username =(this.state.users && 
-//             this.state.users.map((user) => (
-//                 user.username
-//             )));
-//         const userId =
-//    (this.state.users &&     this.state.users.map((user) => (
-//             user.id
-//    )))
-//    const user =
-//    (this.state.users &&     this.state.users.map((user) => (
-//             user
-//    )))
-        
-//         const postUserId = 
-//         this.state.posts &&
-//             this.state.posts.user_id.map((post) => (
-//              Array.post.forEach.call(postUserId, num => {
-//                 return num
-//             })
-//             ))
-        
+  handleUpdate(item) {
+    $.ajax({
+            url: `/users/:user_id/posts/${item.id}`,
+            type: 'PUT',
+            data: { item: item },
+            success: () => {
+                console.log('Updated!');
+                //this.updateItems(item);
+                // callback to swap objects
+            }
+        }
+    )}
     
+    updateItems(item) {
+        var items = this.state.items.filter((i) => { return i.id != item.id });
+        items.push(item);
+    
+        this.setState({items: items });
+    }
+    
+  render() {
+    //         var obj = {};
+    //         const username =(this.state.users &&
+    //             this.state.users.map((user) => (
+    //                 user.username
+    //             )));
+    //         const userId =
+    //    (this.state.users &&     this.state.users.map((user) => (
+    //             user.id
+    //    )))
+    //    const user =
+    //    (this.state.users &&     this.state.users.map((user) => (
+    //             user
+    //    )))
 
-//      // console.log(this.state.users.findIndex(postUserId))
-//         console.log(userId)
-//         console.log(postUserId)
-//         console.log(username)
-//         // console.log(user.get(postUserId.get(0)))
-//         console.log(userId == postUserId ? ({username}) : null)
+    //         const postUserId =
+    //         this.state.posts &&
+    //             this.state.posts.user_id.map((post) => (
+    //              Array.post.forEach.call(postUserId, num => {
+    //                 return num
+    //             })
+    //             ))
+
+    //      // console.log(this.state.users.findIndex(postUserId))
+    //         console.log(userId)
+    //         console.log(postUserId)
+    //         console.log(username)
+    //         // console.log(user.get(postUserId.get(0)))
+    //         console.log(userId == postUserId ? ({username}) : null)
     return (
       <div class="h-screen, m-0, p-0">
         <Header
@@ -176,7 +196,7 @@ class App extends Component {
             {this.state.users &&
               this.state.users.map((user) => (
                 <div className="user">
-                  <Link to={`/user/${user.id}`}>
+                  <Link to={`/profile/${user.id}`}>
                     <h2>{user.username}</h2>
                     <img src={user.url} />
                   </Link>
@@ -187,9 +207,16 @@ class App extends Component {
           <Route
             exact
             path="/posts"
-                    render={(props) => <User_Profile {...props}
-                    userData={this.state.userData}
-                    />}
+            render={(props) => (
+              <User_Profile
+                {...props}
+                userData={this.state.userData}
+                handleSubmit={this.handleSubmit}
+                handleUpdate={this.onUpdate}
+                items={this.state.items}
+                onUpdate={this.handleUpdate}
+              />
+            )}
           >
             {this.state.posts &&
               this.state.posts.map((post) => (
@@ -197,16 +224,17 @@ class App extends Component {
                   <Link to={`/users/${post.id}`}></Link>
                   <h2>{post.title}</h2>
                   <p>{post.description}</p>
-                      <p>{post.body}</p>
-                      <p>Posted by <Link to={`/user/${post.user_id}`}>
-
-                          {post.user.username}
-                          </Link></p>
+                  <p>{post.body}</p>
+                  <p>
+                    Posted by{" "}
+                    <Link to={`/user/${post.user_id}`}>
+                      {post.user.username}
+                    </Link>
+                  </p>
                   <p>Created at {post.created_at}</p>
                   <p>Last Updated {post.updated_at}</p>
                 </div>
               ))}
-           
           </Route>
 
           <Route
@@ -229,12 +257,12 @@ class App extends Component {
             )}
           />
 
-    
-
           <Route
             exact
             path="/category/:id"
-            render={(props) => <Categories {...props} />}
+                    render={(props) => <Categories {...props}
+                    userData={this.state.userData}
+                    />}
           />
         </div>
       </div>
--- client/src/React_pages/User_Profile.jsx
+++ client/src/React_pages/User_Profile.jsx
@@ -1,18 +1,19 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { getUserPosts, addPost, deletePost, updatePost} from "../services/apihelper";
 import { withRouter, Link } from "react-router-dom";
+import $ from 'jquery';
 
 class User_Profile extends Component {
   state = {
     posts: null,
-    addPost: false,
+      addPost: false,
+    editable: false,
     post: {
       title: "",
       description: "",
       body: "",
     },
   };
-
   componentDidMount = async () => {
     const id = this.props.match.params.id;
     const posts = await getUserPosts(id);
@@ -45,6 +46,8 @@ class User_Profile extends Component {
     }));
   };
 
+    
+    
   removePosts = async (id) => {
       const deleted = await deletePost(id);
       this.setState((prevState) => ({
@@ -52,32 +55,84 @@ class User_Profile extends Component {
       }))
   };
 
-    editPosts = async = (id) => {
-        const changePost = await updatePost(id);
-        this.setState((prevState) => ({
-            posts: prevState.posts.filter(onePost => onePost.id != deleted.id)
-        }))
+  getInitialState() {
+    return {editable: false}
+}
+    handleEdit() {
+        if(this.state.editable) {
+            const title = this.refs.title.value;
+            const id = this.props.item.id;
+            const description = this.refs.description.value;
+            const item = {id: id , title: title , description: description};
+            this.props.handleUpdate(item);
+
+            console.log('in handleEdit', this.state.editable, title, description);
+    
+        }
+    this.setState({editable: !this.state.editable})
+}
+    
+    
+onUpdate(item) {
+    this.props.onUpdate(item);
+}
+    
+handleUpdate(item) {
+    $.ajax({
+            url: `/users/:user_id/posts/${item.id}`,
+            type: 'PUT',
+            data: { item: item },
+            success: () => {
+                console.log('Updated!');
+                //this.updateItems(item);
+                // callback to swap objects
+            }
+        }
+    )}
+    
+    updateItems(item) {
+        var items = this.state.items.filter((i) => { return i.id != item.id });
+        items.push(item);
+    
+        this.setState({items: items });
     }
+    // editPosts = async (id) => {
+    //     const changePost = await updatePost(id);
+    //     this.setState((prevState) => ({
+    //         posts: prevState.posts.filter(onePost => onePost.id != deleted.id)
+    //     }))
+    // }
 
   render() {
-    console.log(this.state.posts);
 
+    // const title = this.state.editable ? <input type='text' defaultValue={post.title} /> : <h3>{post.title}</h3>;
+    // const description = this.state.editable ? <input type='text' defaultValue={post.description} />: <h3>{post.description}</h3>;
     return (
       <div>
-        <h1>Your Posts</h1>
-        {this.state.posts &&
+{        <h1>Your Posts</h1>
+}        {this.state.posts &&
           this.state.posts.map((post) => (
             <div className="post">
-              <h3>{post.title}</h3>
-              <h3>{post.description}</h3>
+                  {/* <h3>{post.title}</h3> */}
+                  {this.state.editable ? <input type='text' ref='title' defaultValue={post.title} /> : <h3>{post.title}</h3>}
+                  {this.state.editable ? <input type='text'ref='description' defaultValue={post.description} />: <h3>{post.description}</h3>}
+              {/* <h3>{post.description}</h3> */}
               <h3>{post.body}</h3>
 
-              <button
+              {/* <button
                 placeholder="Edit"
                 onClick={() => {this.editPosts(post.id)}}
               >
-                delete
-              </button>
+                      edit
+              </button> */}
+                  
+              <button onClick={this.handleEdit}>
+  {" "}
+  {this.state.editable ? "Submit" : "Edit"}{" "}
+</button>
+
+
+
               <button
                 placeholder="Delete"
                 onClick={() => {this.removePosts(post.id)}}
