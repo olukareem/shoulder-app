3ea9f7504c9da4d9bec466d7538c12f84f610201
glolorun <63762681+glolorun@users.noreply.github.com>
2021-01-10 21:57:21 -0500

    Updating, error fixing 1/21

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,22 +5,27 @@ before_action :authorize_request, only: [:create, :destroy, :update]
   def index
         @posts = Post.all
         render json: @posts, include: [:user, :categories]
+        # Shows all posts and includes the user and categories
         # @posts = Post.paginate(page: params[:page], per_page: 5)
 
     end
 def get_one_post
     render json: @post
+    # Renders a post
 end
   # GET /posts/1
   def show
     @user = User.find(params[:user_id])
+    #Finds users by the ID number
         render json: @user.posts, include: [:user, :categories]
+        #Shows the post and includes categories
       end
 
   # POST /posts
   def create
     @post = Post.new(post_params)
     @post.user = @current_user
+    #Creates new post for current user
     # @post.push(params)
     puts "params", params
     if @post.save
@@ -32,7 +37,9 @@ end
 
   def add_category
     @post = Post.find(params[:id])
+    #Finds post by Id
     @post.categories << Category.find(params[:category_id])
+    #Associates post with category and finds by category ID
 end
   # PATCH/PUT /posts/1
   def update
--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,33 +1,5 @@
-import api from './apihelper'
-import axios from 'axios'
-// export const signUp = async credentials => {
-//     try {
-//         const resp = await api.post('/sign-up', credentials)
-//         localStorage.setItem('token', resp.data.token)
-//         return resp.data
-//     } catch (error) {
-//         throw error
-//     }
-// }
-
-// export const signInUser = async credentials => {
-//     try {
-//         const resp = await api.post('/sign-in', credentials)
-//         localStorage.setItem('token', resp.data.token)
-//         return resp.data
-//     } catch (error) {
-//         throw error
-//     }
-// }
-
-// export const signOut = async user => {
-//     try {
-//         await localStorage.clear()
-//         return true
-//     } catch (error) {
-//         throw error
-//     }
-// }
+// import api from './apihelper'
+import api from './apiconfig'
 
 export const loginUser = async (loginData) => {
     console.log(api.baseURL)
