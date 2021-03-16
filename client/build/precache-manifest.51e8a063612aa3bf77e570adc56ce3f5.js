0d12866487898f91dd0b78bd3acb4d0d0176daa6
glolorun <63762681+glolorun@users.noreply.github.com>
2021-03-15 22:02:47 -0400

    removed build for heroku

---
+++ client/build/precache-manifest.51e8a063612aa3bf77e570adc56ce3f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7002c5d2635795e562451493ae3420d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b48dbce7e8f7336b77ed",
+    "url": "/static/css/main.d6e445a6.chunk.css"
+  },
+  {
+    "revision": "7f865987a828f984ce16",
+    "url": "/static/js/2.c2e46f81.chunk.js"
+  },
+  {
+    "revision": "005c97f82cee60e20eac659314ea29ec",
+    "url": "/static/js/2.c2e46f81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b48dbce7e8f7336b77ed",
+    "url": "/static/js/main.5422c872.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
